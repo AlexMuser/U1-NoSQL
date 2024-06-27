633595314ab338973bbb8e03755115d1fd0f37c7
vBlackBearr <Luispuentes.pgo@outlook.com>
2024-06-26 22:44:43 -0600

    Chingos de cambios

--- create_arturo.sql
+++ create_arturo.sql
@@ -18,49 +18,6 @@ CREATE DATABASE BD_Pruebas;
 
 USE BD_Pruebas;
 
--- Tabla para tokens de correos
-CREATE TABLE tokens_correos (
-    correo VARCHAR(100) NOT NULL PRIMARY KEY,
-    token VARCHAR(255) NOT NULL,
-    fecha_generacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    correo_validado BOOLEAN DEFAULT FALSE,
-    registro_completado BOOLEAN DEFAULT FALSE,
-    CONSTRAINT CHK_correo CHECK (correo REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-) ENGINE=InnoDB;
-
-
--- Trigger para asegurar que el correo no se repita si correo_validado = true y registro_completado = true
--- Y si ya existe un registro con el correo pero no esta correo_validado o registro_completado, permitir la inserción pero antes eliminar el registro anterior
-DELIMITER //
-
-CREATE TRIGGER before_insert_tokens_correos
-BEFORE INSERT ON tokens_correos
-FOR EACH ROW
-BEGIN
-    DECLARE v_count INT;
-
-    -- Verificar si el correo ya está registrado con correo_validado y registro_completado en true
-    SELECT COUNT(*)
-    INTO v_count
-    FROM tokens_correos
-    WHERE correo = NEW.correo
-      AND correo_validado = TRUE
-      AND registro_completado = TRUE;
-
-    -- Si existe al menos un registro, no permitir la inserción y lanzar un error
-    IF v_count > 0 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Este correo ya ha sido registrado para esta convocatoria';
-    ELSE
-        -- Eliminar el registro anterior en caso de que exista
-        DELETE FROM tokens_correos
-        WHERE correo = NEW.correo;
-    END IF;
-END//
-
-DELIMITER ;
-
-
 
 -- Tabla convocatorias
 CREATE TABLE convocatorias (
@@ -75,6 +32,53 @@ CREATE TABLE convocatorias (
     CONSTRAINT CHK_numero_convocatoria CHECK (numero_convocatoria IN (1, 2)),
     CONSTRAINT CHK_anio CHECK (anio > 0)
 ) ENGINE=InnoDB;
+
+
+-- Tabla para tokens de correos
+CREATE TABLE tokens_correos (
+    id_token_correo INT AUTO_INCREMENT PRIMARY KEY,
+    correo VARCHAR(100) NOT NULL,
+    token VARCHAR(255),
+    fecha_generacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    correo_validado BOOLEAN DEFAULT FALSE,
+    registro_completado BOOLEAN DEFAULT FALSE,
+    convocatoria_id INT NOT NULL,
+    CONSTRAINT CHK_correo CHECK (correo REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+    FOREIGN KEY (convocatoria_id) REFERENCES convocatorias(id_convocatoria)
+) ENGINE=InnoDB;
+
+
+-- Trigger para asegurar que el correo no se repita si correo_validado = true y registro_completado = true
+-- y generar un uuid en caso de se haga la insercion
+DELIMITER //
+
+CREATE TRIGGER before_insert_tokens_correos
+BEFORE INSERT ON tokens_correos
+FOR EACH ROW
+BEGIN
+    DECLARE v_count INT;
+
+    -- Verificar si el correo ya está registrado con correo_validado y registro_completado en true
+    SELECT COUNT(*)
+    INTO v_count
+    FROM tokens_correos
+    WHERE correo = NEW.correo
+      AND correo_validado = TRUE
+      AND registro_completado = TRUE
+      AND convocatoria_id = NEW.convocatoria_id;
+
+    -- Si existe al menos un registro, no permitir la inserción y lanzar un error
+    IF v_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Este correo ya ha sido registrado para esta convocatoria';
+    ELSE
+        SET NEW.token = SUBSTRING(uuid(), 1, 8);
+    END IF;
+END//
+
+DELIMITER ;
+
+
  
 -- Tabla aspirantes
 CREATE TABLE aspirantes (
@@ -82,13 +86,14 @@ CREATE TABLE aspirantes (
     id_convocatoria INT,
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(100) NOT NULL,
+    id_token_correo INT,
     curp VARCHAR(18) NOT NULL,
     CONSTRAINT UK_correo UNIQUE (correo),
     CONSTRAINT UK_curp UNIQUE (curp),
     CONSTRAINT CHK_correo CHECK (correo REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
     CONSTRAINT CHK_curp CHECK (curp REGEXP '^[A-Z]{4}[0-9]{6}[HM][A-Z]{5}[0-9]{2}$'),
     FOREIGN KEY (id_convocatoria) REFERENCES convocatorias(id_convocatoria),
-    FOREIGN KEY (correo) REFERENCES tokens_correos(correo)
+    FOREIGN KEY (id_token_correo) REFERENCES tokens_correos(id_token_correo)
 ) ENGINE=InnoDB;
 
 -- Tabla catalogo_carreras
